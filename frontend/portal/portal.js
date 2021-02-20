207f8fb6846be57c1fb592b4cbd0b055dd04565b
Marc Ernstberger <marc@marc1307.de>
2021-02-20 02:12:54 +0100

    updated frontend

--- frontend/portal/portal.js
+++ frontend/portal/portal.js
@@ -12,28 +12,92 @@ function requestAccess(e) {
     console.log("Host: "+host)
 
     url = api_requestAccess
-    $.ajax({
-        url: url,
-        method: "POST",
-        data: { sessionId: sessionId, host: host },
-        error: function(xhr, textStatus, error) {
-            alert(xhr.textStatus)
-        }
-    }).done(
-        function(data) {
-            alert(data)
-        }
-    )
+    if (window.location.protocol !== "file:") {
+        $.ajax({
+            url: url,
+            method: "POST",
+            data: { sessionId: sessionId, host: host },
+            error: function(xhr, textStatus, error) {
+                console.log(xhr.textStatus)
+            }
+        }).done(
+            function(data) {
+                console.log(data)
+            }
+        )
+    } else {
+        console.log("Warning Protocol file:")
+    }
 }
 
 
 // onLoad
 $(document).ready(
     function() {
-        console.log("rdy")
+        console.log("document.ready")
+        if (window.location.protocol !== "file:") {
+            var intervall_getStatus = setInterval(function() {
+                $.ajax({
+                    url: "/api/status",
+                    error: function(xhr, textStatus, error) {
+                        console.log(xhr.textStatus)
+                    }
+                }).done(
+                    function (data) {
+                        if(data.success) {
+                            if (data.data.ISE !== false) {
+                                $("#mac").text(data.data.ISE.mac)
+                                $("#ip4").text(data.data.ISE.framed_ip_address)
+                            }
+
+                            if (data.data.PreAuth !== false) {
+                                if (data.data.PreAuth.success) {
+                                    $("#authres1").text("Found Device: "+data.data.PreAuth.DeviceName)
+                                } else {
+                                    $("#authres1").text(data.data.PreAuth.msg)
+                                    $("#authProgress").css("background-color", "red")
+                                }
+                            }
+
+                            if (data.data.Auth !== false) {
+                                if (data.data.Auth.success) {
+                                    $("#authres2").text("Sent Push successfully")
+                                }
+                            }
+
+                            if (data.data.AuthStatus !== false) {
+                                if(data.data.AuthStatus.success) {
+                                    $("#authres3").text(data.data.AuthStatus.msg)
+                                    $("#authProgress").css("background-color", "green")
+                                } else {
+                                    $("#authres3").text(data.data.AuthStatus.msg)
+                                    $("#authProgress").css("background-color", "red")
+                                }
+                            }
+
+                            // Redirect Client if CoA is sent to NAD
+                            if (data.data.detail.IseCoaSent) {
+                                $("#authres4").text("Guest Access approved. Please wait")
+                                setTimeout(function() {
+                                    followRedirect()
+                                }, 1500)
+                            }
+                        }
+
+                        $('#status').text(data.status)
+                        $('pre#detail').text(JSON.stringify(data.detail, undefined, 2))
+                    }
+                )
+            }, 1000)
+        }
+
+
+        // redirect
+        qs = getQueryParams(window.location.search)
+        $('a#redirect').attr("href", qs["redirect_url"])
+
     }
 )
-
 
 // Stuff
 function getQueryParams(qs) {
@@ -48,4 +112,9 @@ function getQueryParams(qs) {
     }
 
     return params;
+}
+
+function followRedirect() {
+    qs = getQueryParams(window.location.search)
+    window.location.href = qs['redirect_url']
 }
