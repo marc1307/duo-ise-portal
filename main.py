ffc326093c2089a85f337419334f2c00c249bf18
Marc Ernstberger <marc@marc1307.de>
2021-02-24 21:41:05 +0100

    cleanup & fixed

--- main.py
+++ main.py
@@ -19,28 +19,6 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def portal():
     with open('frontend/portal/index.html','r') as file:
         index = file.read()
-    
-    # sessionId = request.args.get('sessionId', default = 'na', type = str)
-    # session['sessionId'] = sessionId
-    
-    # try:
-    #     type(session['detail'])
-    # except KeyError:
-    #     print("session['detail'] KeyError")
-    #     session['detail'] = initSession()
-
-
-    # IseSessionNotFound = True
-    # while (IseSessionNotFound):
-    #     sessionInfo = ise.getSessionInfo(sessionId)
-    #     if sessionInfo != False:
-    #         IseSessionNotFound = False
-    #         session['foundIseSession'] = True
-    #         print("------session['foundIseSession'] -> {}".format(session['foundIseSession']))
-    #         session['ISE'] = sessionInfo
-    #     else:
-    #         time.sleep(1)
-
     return index
 
 @app.route('/portal/<resource>')
@@ -99,19 +77,26 @@ def handle_auth(data):
     print('auth - received host: ' + data)
     
     host = data ###### sanatize me
-
+    session['host'] = host
 
     # Do Preauth
     preauth = duo.preauth(username=host)
-    print(json.dumps(preauth, indent=4))
     if preauth["response"]["result"] == "deny":
         io.emit('auth', {"module": "preauth", "success": False, "msg": preauth["response"]['status_msg']}, room=session['socketsid'])
     else:
-        io.emit('auth', {"module": "preauth", "success": True}, room=session['socketsid'])
+        deviceCount = {"phone": 0, "token": 0}
+        for device in preauth["response"]["devices"]:
+            if device["type"] == "phone":
+                deviceCount['phone'] = deviceCount['phone'] + 1
+            if device["type"] == "token":
+                deviceCount['token'] = deviceCount['token'] + 1
+        io.emit('auth', {"module": "preauth", "success": True, "count": deviceCount}, room=session['socketsid'])
         # Search for pushable phone
         for device in preauth["response"]["devices"]:
             if device["type"] == "phone":
                 if 'push' in device["capabilities"]:
+                    if 'mobile_otp' in device["capabilities"]:
+                        io.emit('auth', {"module": "passcode", "success": True}, room=session['socketsid'])
                     # Send Push to Authentication
                     io.emit('auth', {"module": "preauth2", "success": True, "device": device['display_name']}, room=session['socketsid'])
                     auth = duo.auth_push(username=host, device=device['device'], mac=session['ISE']['mac'])
@@ -141,42 +126,32 @@ def handle_auth(data):
                                         io.emit('auth', {"module": "ise-addEig", "success": False}, room=session['socketsid'])
                             else:
                                 print("Request Allowed ({} -> {})".format(r['response']['status'], r['response']['status_msg']))
-            
-                        
-                            
+            break
 
 
+@io.on('auth_passcode') # Send ISE Session ID
+def handle_auth_passcode(data):
+    print('auth_passcode - received host: ' + data)
 
+    token = data
+    
+    r = duo.auth_passcode(session['host'], token)
+    if (r['response']['result'] == "allow"):
+        io.emit('auth', {"module": "auth_status", "success": True, "state": r['response']['result'], "msg": r['response']['status_msg']}, room=session['socketsid'])
+        if ise.authorizeGuest(session['ISE']['mac']):
+            io.emit('auth', {"module": "ise-addEig", "success": True}, room=session['socketsid'])
+            if ise.sendReauthCoa(server=session['ISE']['server'], mac=session['ISE']['mac']):
+                io.emit('auth', {"module": "ise-coa", "success": True}, room=session['socketsid'])
+        else:
+            io.emit('auth', {"module": "ise-addEig", "success": False}, room=session['socketsid'])
+    else:
+        io.emit('auth', {"module": "auth_status", "success": True, "state": r['response']['result'], "msg": r['response']['status_msg']}, room=session['socketsid'])
 
-
-
-
-def emitStatus(event, module, msg=""):
-    io.emit(event, {"success": True, "module": module, "msg": msg}, room=session['socketsid'])
-
-def emitError(event, msg):
-    io.emit(event, {"success": False, "error": msg}, room=session['socketsid'])
 
 @app.route('/')
 def hello():
     return 'Ready to rock :)'
 
-def apiResponseSuccess(data):
-    if (type(data) == dict):
-        out = {
-            "success": True,
-            "data": data
-        }
-        return json.dumps(out)
-    else:
-        exit("apiResponse(): Input Validation failed [data={}]".format(str(type(data))))
-
-def apiResponseError(msg):
-    out = {
-        "success": False,
-        "error": msg
-    }
-    return json.dumps(out)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8443)
